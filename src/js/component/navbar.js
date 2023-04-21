486daa306e813f6ff5b6e56abd9f7a656990843f
José Antonio Verdugo Ardila <joseantonioverdugoardila@gmail.com>
2023-04-21 09:02:08 +0000

    añadiendo efectos y logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.css";
+import logo from "../../img/starWarsLogo.png"
 
 export const Navbar = () => {
 	return (
 		<nav className="Navbar">
 			<Link className="Navbar-link" to="/">
-			<h1 className="Navbar-h1">Star Wars Blog</h1>
+			<h1 className="Navbar-h1"><img className="Navbar-img" src= {logo}/></h1>
 			</Link>
 			<div className="ml-auto">
 			</div>
