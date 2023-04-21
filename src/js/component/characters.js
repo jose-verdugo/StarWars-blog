318a87a266213a77e0d6fdfd85a8411c095f54e5
José Antonio Verdugo Ardila <joseantonioverdugoardila@gmail.com>
2023-04-21 11:05:48 +0000

    añadiendo fav

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext,useState, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import "../../styles/card.css";
 import { TailSpin } from "react-loader-spinner";
@@ -6,6 +7,7 @@ import { TailSpin } from "react-loader-spinner";
 export const Character = () => {
   const [people, setPeople] = useState(null);
   const [peoplePage, setPeoplePage] = useState(1);
+  const { store, actions } = useContext(Context);
 
   const urlPeople = `https://www.swapi.tech/api/people?page=${peoplePage}&limit=5`;
 
@@ -27,6 +29,12 @@ export const Character = () => {
     getPeople();
   }, [peoplePage]);
 
+  const handleLikeClick = () => {
+    const likeButton = document.querySelector('.Card-like');
+    likeButton.classList.toggle('Card-like--red');
+  };
+  
+
   return (
     <section className="Section">
       <h2 className="Section-title">Characters</h2>
@@ -35,34 +43,48 @@ export const Character = () => {
           people.map((person) => (
             <div className="Card" key={person.uid}>
               <div className="Card-left">
-              <img className="Card-img" src={`https://starwars-visualguide.com/assets/img/characters/${person.uid}.jpg`} alt={person.properties.name} />
+                <img
+                  className="Card-img"
+                  src={`https://starwars-visualguide.com/assets/img/characters/${person.uid}.jpg`}
+                  alt={person.properties.name}
+                />
               </div>
               <div className="Card-rigth">
-              <h3 className="Card-name">{person.properties.name}</h3>
-              <ul className="Card-ul">
-                <li className="Card-li"><b>Gender</b>: {person.properties.gender}</li>
-                <li className="Card-li">
-                  <b>Hair Color</b>: {person.properties.hair_color}
-                </li>
-                <li className="Card-li">
-                  <b>Eye Color</b>: {person.properties.eye_color}
-                </li>
-              </ul>
-              <div className="Card-buttons">
-              <Link to={`/characterView${person.uid}`}>
-                <button className="Card-btn">Learn More!</button>
-              </Link>
-              <svg
-                className="Card-like"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-              </svg>
-              </div>
+                <h3 className="Card-name">{person.properties.name}</h3>
+                <ul className="Card-ul">
+                  <li className="Card-li">
+                    <b>Gender</b>: {person.properties.gender}
+                  </li>
+                  <li className="Card-li">
+                    <b>Hair Color</b>: {person.properties.hair_color}
+                  </li>
+                  <li className="Card-li">
+                    <b>Eye Color</b>: {person.properties.eye_color}
+                  </li>
+                </ul>
+                <div className="Card-buttons">
+                  <Link to={`/characterView${person.uid}`}>
+                    <button className="Card-btn">Learn More!</button>
+                  </Link>
+                  <svg
+                    className="Card-like"
+                    onClick={() =>{
+                      actions.selectId(person);
+                      actions.addFavorite();
+                      handleLikeClick();
+                    }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           ))
@@ -80,14 +102,14 @@ export const Character = () => {
         )}
       </main>
       <footer className="Section-footer">
-        {peoplePage > 1 ?(
+        {peoplePage > 1 ? (
           <button
-          className="Card-page"
-          onClick={() => setPeoplePage(peoplePage - 1)}
-        >
-          Previous Page
-        </button>
-        ): null }
+            className="Card-page"
+            onClick={() => setPeoplePage(peoplePage - 1)}
+          >
+            Previous Page
+          </button>
+        ) : null}
         <button
           className="Card-page"
           onClick={() => setPeoplePage(peoplePage + 1)}
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,25 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			favorites: [],
+			idCharacter: {},
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			selectId: (character) => {
+				setStore({ idCharacter: character });
+				console.log(getStore().idCharacter)
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			addFavorite: () => {
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+				const newFavorite = store.idCharacter;
+				setStore({ favorites: [...store.favorites, newFavorite] });
+				console.log(getStore().favorites)
+			},
+			removeFavorite: (fav) => {
+				const store = getStore();
+				const updatedArray = store.favorites.filter((favorite) => favorite !== fav);
+				setStore({ favorites: updatedArray });
+			},
 		}
 	};
 };
